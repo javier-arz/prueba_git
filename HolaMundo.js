9b9aef42050398ad8943caff37d116812f437bd4
javier-arz <javier-arz@hotmail.com>
2016-12-18 11:51:49 -0500

    Se agrega comentario que quedaría solo en la rama test

--- HolaMundo.js
+++ HolaMundo.js
@@ -2,6 +2,8 @@ console.log('Hola mundo!!');
 
 //ki comentario ...
 
+// Comentario que quedaría solo en la rama test
+
 var http = require("http");
 http.createServer(function(request, response) {
 response.writeHead(200, {"Content‐Type": "text/html"});
