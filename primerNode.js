4156ee8e9fe816576a06ec669cbe9ef07d83fbb6
javier-arz <javier-arz@hotmail.com>
2016-12-18 10:54:08 -0500

    Se agrega archivo primerNode.js

---
+++ primerNode.js
@@ -0,0 +1,14 @@
+var http = require("http");
+
+http.createServer(function  (request, response) {
+	// Enviar la cabecera http
+	//Http status :200 : OK
+	// Content type : text/plain
+	response.writeHead(200, {'Content-Type' : 'text/plain'});
+
+	//Enviar el cuerpo de la respuesta como Hola mundo y escuchar por el puerto 3000
+	response.end('Hola World\n');
+}).listen(3000);
+
+//Imprimir el mensaje de ejecución en consola 
+console.log('Servidor corriendo en localhost:3000');
